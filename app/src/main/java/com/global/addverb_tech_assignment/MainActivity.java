13cfba054a935abefb69f7c588172de466bbbd66
devmehta <devmehta162@gmail.com>
2022-01-13 19:30:57 +0530

    Dev Mehta Assignment (Addverb technologies)

--- app/src/main/java/com/global/addverb_tech_assignment/MainActivity.java
+++ app/src/main/java/com/global/addverb_tech_assignment/MainActivity.java
@@ -54,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
         loadingTxt = findViewById(R.id.loadingTxt);
         displayRegionModelArrayList = new ArrayList<>();
 
+        //
         boolean isInternetConnected = internetIsConnected();
 
         // to recall the api , if there is some issue
